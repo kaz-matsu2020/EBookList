3c330036ffda842c7b9b73cd643eae9f32f89652
kaz-matsu2020 <kazuo.matsushima620@gmail.com>
2024-04-23 10:59:26 +0900

    重複を確認するためのDAOを作成

---
+++ main/java/DAO/UserIdOverlapCheckDAO.java
@@ -0,0 +1,69 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * ユーザー登録するためのDAO
+ * @author kazuo
+ */
+
+public class UserIdOverlapCheckDAO {
+	private final String DB_NAME = "EBookList";
+	private final String PROPATIES = "?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo";
+	private final String JDBC_URL = "jdbc:mysql://エンドポイント/" + DB_NAME + PROPATIES;
+	private final String DB_USER = "ユーザー名";
+	private final String DB_PASS = "パスワード";
+	
+	/**
+	 * userRegisterメソッド
+	 * @param userId 文字列
+	 * @return すでにユーザーIDがあるかどうか真偽値
+	 */
+	
+	public boolean userIdOverlapCheck(String userId) {
+		boolean overlap = false;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCを読み込めませんでした");
+		}
+		
+		Connection con = null;
+		try {
+			// データベースに接続
+			con = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS); 
+			
+			// sql処理を記述
+			String sql = "select user_id from users where user_id = ?";
+			PreparedStatement pStmt = con.prepareStatement(sql);
+			
+			// sql文中の｢?｣に使用する値を設定してSQL文を完成
+			pStmt.setString(1, userId);
+			
+			// selectを実行
+			ResultSet rs = pStmt.executeQuery();
+			if(!rs.next()) {
+				overlap = true;
+			} 
+			return overlap;
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			// データベース接続の切断
+			if (con != null) {
+				try {
+					con.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
